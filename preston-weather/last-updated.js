03114202ade231a58e603acb38a862e4ef6f09e3
Ryan Bey <bey15002@byui.edu>
2021-10-06 21:28:30 -0600

    formatted last updated js, finalized styles

---
+++ preston-weather/last-updated.js
@@ -0,0 +1,22 @@
+let d = new Date();
+
+// Keeps copyright date up to date
+document.getElementById('current-year').innerText = "\u00A9" + d.getFullYear() + " Ryan Bey";
+
+// Updates "Last Updated" on mist pages with correct formatting
+let months = [ "January", "February", "March",
+               "April", "May", "June", "July",
+               "August", "September", "October",
+               "November", "December" ];
+
+let days = [ "Sunday", "Monday", "Tuesday",
+             "Wednesday", "Thursday",
+            "Friday", "Saturday" ];
+
+let date = new Date();
+let monthName = months[date.getMonth()];
+let dayName = days[date.getDay()];
+let year = date.getFullYear();
+let lastUpdated = `${dayName}, ${date.getDate()} ${monthName} ${year}`;
+
+document.getElementById("last-updated").innerHTML = lastUpdated;
