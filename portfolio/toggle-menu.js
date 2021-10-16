75921939564055e6e955b644ee3f4666c36a4289
Ryan Bey <bey15002@byui.edu>
2021-10-15 23:39:46 -0600

    finished responsive resizing, got email form working

---
+++ portfolio/toggle-menu.js
@@ -0,0 +1,4 @@
+const hambutton = document.querySelector('.menu');
+const navigation = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {navigation.classList.toggle('responsive')}, false);
