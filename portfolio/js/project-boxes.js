ed120c7387834124550aec6a5785ef1fdb552397
Ryan Bey <bey15002@byui.edu>
2022-05-10 22:38:19 -0600

    improved project btn animation and link navigation

--- portfolio/js/project-boxes.js
+++ portfolio/js/project-boxes.js
@@ -2,28 +2,28 @@ let allProjectBoxes = document.querySelectorAll('.projects-box');
 
 allProjectBoxes.forEach((projectBox) => {
    projectBox.addEventListener('mouseenter', () => {
+      // Darken & blur image
+      let projectImg = projectBox.querySelector('.box-img');
+      projectImg.classList.add('project-img-darken');
+      projectImg.classList.remove('project-img-lighten');
+
+      // Create Button
+      let link = document.createElement('a');
+      link.href = `projects.html#${projectBox.id}`;
+      link.id = 'details-btn';
+      link.classList.add('project-img-btn-show');
+      let btn = document.createElement('button');
+      btn.innerText = 'VIEW DETAILS';
+
+      // Append Button (give time for possible duplicate buttons to be removed)
       setTimeout(() => {
       if (!document.getElementById('details-btn')) {
-            let projectImg = projectBox.querySelector('.box-img');
-
-            // Darken & blur image
-            projectImg.classList.add('project-img-darken');
-            projectImg.classList.remove('project-img-lighten');
-
-            // Create Button
-            let link = document.createElement('a');
-            link.href = `projects.html#${projectBox.id}`;
-            link.id = 'details-btn';
-            link.classList.add('project-img-btn-show');
-            let btn = document.createElement('button');
-            btn.innerText = 'VIEW DETAILS';
-
-            // Append Button
             projectBox.appendChild(link);
             link.appendChild(btn);
          }
-      }, 200);
+      }, 151);
    });
+
    projectBox.addEventListener('mouseleave', () => {
       let projectImg = projectBox.querySelector('.box-img');
 
@@ -34,11 +34,11 @@ allProjectBoxes.forEach((projectBox) => {
       // Animate button hide
       let link = document.getElementById('details-btn');
       link.classList.remove('project-img-btn-show'); // Remove class that showed button
-      link.classList.add('project-img-btn-hide'); // Add class to animate hiding button
+      link.classList.add('project-img-btn-hide');    // Add class to animate hiding button
 
       // Remove button
       setTimeout(() => {
-         link.remove(); // Wait until animation is completed (300ms)
-      }, 200);
+         link.remove(); // Wait until animation is almost completed
+      }, 150);
    });
 });
